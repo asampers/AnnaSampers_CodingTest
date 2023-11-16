b60a81c956094a4c077791495e1370140a35d6d1
nanarama <annasampers@gmail.com>
2023-11-16 12:10:18 -0600

    Add validation testing

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(name: "TestItem")
+  }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end  
+
+    it "is not valid without name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
