2b96a492784e7b4e4863de909c48eb6814a6deac
nanarama <annasampers@gmail.com>
2023-11-16 11:51:55 -0600

    Add methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+
+  def soft_delete
+    update_attribute(:deleted_at, Time.now)
+  end
+
+  def restore
+    update_attribute(:deleted_at, nil)
+  end
 end
